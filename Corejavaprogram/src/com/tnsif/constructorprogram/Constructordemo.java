e98b110333bda0c4457ff68f00a54c9238a7110d
SHOBHA <SHOBHA@LAPTOP-DG2QHHIV>
2026-06-08 18:41:02 +0530

    constructor program

---
+++ Corejavaprogram/src/com/tnsif/constructorprogram/Constructordemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.constructorprogram;
+class User1{
+    String name;
+    int age;
+
+    User1(){
+        name="Guest";
+        age=5;
+        System.out.println("Guest profile created");
+    }
+    User1(String name,int age){
+        this.name=name;
+        this.age=age;
+        System.out.println("User created: name "+name+" Age :"+age);
+    }
+}
+public class Constructordemo {
+    public static void main(String[] args) {
+        User1 u=new User1();
+        User1 u1=new User1("alice",25);
+    }
+}
