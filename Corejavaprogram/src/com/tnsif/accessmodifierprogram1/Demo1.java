9446f5d12bc2a8cb7a7d21ec4581b94bda654ca8
SHOBHA <SHOBHA@LAPTOP-DG2QHHIV>
2026-06-08 17:52:55 +0530

    packages basic demo class

---
+++ Corejavaprogram/src/com/tnsif/accessmodifierprogram1/Demo1.java
@@ -0,0 +1,22 @@
+package com.tnsif.accessmodifierprogram1;
+
+public class Demo1 {
+	
+	
+	        
+	        String h="roshn";
+	        String H="roshn";
+	        
+	public static void main(String[]args) {// main method
+	        
+	        Publicdemo p=new Publicdemo();
+	        
+	        System.out.println(p.a);
+	        //System.out.println(p.b);
+	        System.out.println(p.s);
+	        
+	        
+	        System.out.println("welcome to java");
+	}
+	}
+
